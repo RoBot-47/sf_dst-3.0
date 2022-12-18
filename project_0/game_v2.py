dc73c201a1d47a97879b762a9004c9b494b57e41
RoBot47 <ro.bot47@mail.ru>
2022-12-18 19:31:53 +0800

    Edit_Game_optimization

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -37,8 +37,6 @@ def score_game(random_predict) -> int:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls)) # находим среднее количество попыток
-
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
 
